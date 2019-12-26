5db8c172709ca2f8ce0e9f13474707aa2068ea22
m18611694189@163.com <m18611694189@163.com>
2019-12-26 11:21:53 +0800

    1226-1

---
+++ demo15/getrank2.py
@@ -0,0 +1,60 @@
+import sys
+from pymongo import MongoClient
+
+mongo = MongoClient(host='127.0.0.1',port=27017).shiyanlou
+
+def get_rank(user_id):
+    client = MongoClient(host='127.0.0.1',port=27017)
+    db = client.shiyanlou
+
+    # 统计用户的总分数和总提交时间
+    pieline = [
+        # 选取指定用户的记录
+        {'$match':{'user_id':user_id}},
+        {'$group':{
+            '_id':'$user_id',
+            'total_score':{'$sum':'$score'},
+            'total_time':{'$sum':'$submit_time'}
+        }}
+    ]
+
+    results = list(db.contests.aggregate(pipeline=pieline))
+    # print(results)
+    if len(results) == 0 :
+        return 0,0,0
+    data = results[0]
+    print('data:',data)
+    # 计算指定用于的排名信息
+    pipeline = [
+        # 分组计算所有用户的总分数和总时间
+        {'$group':{
+            '_id':'$user_id',
+            'total_score':{'$sum':'$score'},
+            'total_time':{'$sum':'$submit_time'}
+        }},
+        # 把上一步筛选出来的文档归为一个分组（_id 为 None）并计算总数
+
+        {'$group':{'_id':None,'count':{'$sum':1}}}
+    ]
+
+    results = list(db.contests.aggregate(pipeline=pipeline))
+    print('results',results)
+    if len(results) > 0 :
+        rank = results[0]['count'] + 1
+    else:
+        rank = 1
+    return  rank , data['total_score'] , data['total_time']
+if __name__ == '__main__':
+
+    '''
+    1. 判断参数格式是否符合要求
+    2. 获取 user_id 参数
+    '''
+    if len(sys.argv) != 2:
+        print('parameter error')
+        sys.exit(1)
+
+    user_id = int(sys.argv[1])
+    # 根据用户 ID 获取用户排名，分数和时间
+    userdata = get_rank(user_id)
+    print(userdata)
