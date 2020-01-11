d82f263d5b0063007bdee119f98bfc566051ca16
m18611694189@163.com <m18611694189@163.com>
2020-01-11 23:12:41 +0800

    2020-1-11-2

--- demo26/simpledu/初始化.py
+++ demo26/simpledu/初始化.py
@@ -1,12 +1,16 @@
-from demo26.manage import  db , User , Course
+from simpledu.modes import User , Course
+from demo26.manage import  app,db
 
-db.create_all()
+db.create_all(app=app)
+app.app_context().push()
+def my_function():
+    with app.app_context():
+        user = User(username='admin')
+        course1 = Course(name='python course', author=user)
+        course2 = Course(name='java course', author=user)
 
-user = User(username='admin')
-course1 = Course(name='python course',author=user)
-course2 = Course(name='java course',author=user)
-
-db.session.add(user)
-db.session.add(course1)
-db.session.add(course2)
-db.session.commit()
+        db.session.add(user)
+        db.session.add(course1)
+        db.session.add(course2)
+        db.session.commit()
+my_function()
